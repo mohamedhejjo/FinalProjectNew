75de0e6b96a63ba57d63eaacae9bfc658f0cc971
hp <ia.amassi@alaqsa.edu.ps>
2022-06-06 14:35:16 +0200

    تعديل بسيط على price

--- app/src/main/java/com/example/finalprojectnew/user/Categories/Price.java
+++ app/src/main/java/com/example/finalprojectnew/user/Categories/Price.java
@@ -1,5 +1,6 @@
 package com.example.finalprojectnew.user.Categories;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,13 +9,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.finalprojectnew.Class.Product;
 import com.example.finalprojectnew.Finish;
 import com.example.finalprojectnew.R;
+import com.example.finalprojectnew.admin.Colthesa.deletecolthes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Price extends AppCompatActivity {
-
+String id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,23 +32,37 @@ public class Price extends AppCompatActivity {
         TextView pro=findViewById(R.id.pricepro);
         TextView female=findViewById(R.id.prfema);
         Button btn=findViewById(R.id.buy);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getBaseContext(), Finish.class);
-                startActivity(i);
-            }
-        });
-
         ImageView image=findViewById(R.id.imageprice);
         Intent intent=getIntent();
         Product csp=(Product)intent.getSerializableExtra("finalu");
         name.setText(csp.getName());
+        id=csp.getId();
         price.setText(csp.getPrice());
         pro.setText(csp.getProduct());
 //        image.setImageResource(csp.getImage());
         female.setText(csp.getSex());
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                Intent i = new Intent(getBaseContext(), Finish.class);
+                startActivity(i);
+                FirebaseDatabase db=FirebaseDatabase.getInstance();
+                DatabaseReference dr = db.getReference("addcolthes");
+                dr.child(id).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+
+                            Toast.makeText(getApplicationContext(), "sold", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(), "not sold", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+            }
+        });
 
 
     }
